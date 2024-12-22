330d1d30ddb0d38e325ac82d730e95771d7acd42
Anurag bhoite <anuragcr7778@gmail.com>
2024-12-22 16:45:42 +0530

    Auth

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -18,6 +18,7 @@ import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { createAccount } from "@/lib/actions/user.actions";
+import OTPModal from "./OTPModal";
 
 type FormType = "Sign In" | "Sign Up";
 const authFormSchema = (formType: FormType) => {
@@ -31,9 +32,9 @@ const authFormSchema = (formType: FormType) => {
 };
 
 const AuthForm = ({ type }: { type: FormType }) => {
-  const [ loading, setIsLoading ] = useState(false);
-  const [ errorMessage, setErrorMessage ] = useState<string | null>(null);
-  const [ accountId, setAccountId ] = useState(null);
+  const [loading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [accountId, setAccountId] = useState(null);
 
   const formSchema = authFormSchema(type);
   const form = useForm<z.infer<typeof formSchema>>({
@@ -51,98 +52,106 @@ const AuthForm = ({ type }: { type: FormType }) => {
     try {
       const user = await createAccount({
         fullname: values.fullName || "",
-        email: values.email
+        email: values.email,
       });
-  
+
       setAccountId(user.accountId);
     } catch (error) {
       setErrorMessage("Failed to create account please try again");
-    } finally{
+    } finally {
       setIsLoading(false);
     }
   };
 
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="auth-form">
-        <h1 className="form-title">
-          {type === "Sign In" ? "Sign In" : "Sign Up"}
-        </h1>
-        {type === "Sign Up" && (
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="auth-form">
+          <h1 className="form-title">
+            {type === "Sign In" ? "Sign In" : "Sign Up"}
+          </h1>
+          {type === "Sign Up" && (
+            <FormField
+              control={form.control}
+              name="fullName"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="shad-form-item">
+                    <FormLabel className="shad-form-label">Full Name</FormLabel>
+
+                    <FormControl>
+                      <Input
+                        placeholder="John Doe"
+                        className="shad-input"
+                        {...field}
+                      />
+                    </FormControl>
+                  </div>
+
+                  <FormMessage className="shad-form-message" />
+                </FormItem>
+              )}
+            />
+          )}
           <FormField
-            control={form.control}
-            name="fullName"
-            render={({ field }) => (
-              <FormItem>
-                <div className="shad-form-item">
-                <FormLabel className="shad-form-label">Full Name</FormLabel>
-
-                <FormControl>
-                  <Input placeholder="John Doe" 
-                  className="shad-input"
-                  {...field} />
-                </FormControl>
-                </div>
-
-                <FormMessage className="shad-form-message"/>
-              </FormItem>
-            )}
-          />
-        )}
-        <FormField
             control={form.control}
             name="email"
             render={({ field }) => (
               <FormItem>
                 <div className="shad-form-item">
-                <FormLabel className="shad-form-label">Email</FormLabel>
+                  <FormLabel className="shad-form-label">Email</FormLabel>
 
-                <FormControl>
-                  <Input placeholder="abc@xyz.com" 
-                  className="shad-input"
-                  {...field} />
-                </FormControl>
+                  <FormControl>
+                    <Input
+                      placeholder="abc@xyz.com"
+                      className="shad-input"
+                      {...field}
+                    />
+                  </FormControl>
                 </div>
 
-                <FormMessage className="shad-form-message"/>
+                <FormMessage className="shad-form-message" />
               </FormItem>
             )}
           />
 
-        <Button 
-        type="submit"
-        className="form-submit-button"
-        disabled={loading}
-        >
-          {!loading ? type === "Sign In" ? "Sign In" : "Sign Up" : <></>}
-          {loading && (
-            <Image 
-            src="/assets/icons/loader.svg"
-            alt="loading"
-            width={24}
-            height={24}
-            className="ml-2 animate-spin"
-            />
-          )}
-        </Button>
-        {
-          errorMessage && (<p className="error-message">
-            *{errorMessage}
-          </p>)
-        }
-
-        <div className="body-2 flex justify-center">
-          <p className="text-light-100">
-            {type === "Sign In" ? " Don't have an account?" : "Already have an account?"}
-          </p>
-          <Link href={type === "Sign In" ? "/sign-up" : "/sign-in"}
-          className="ml-1 font-medium text-brand"
+          <Button
+            type="submit"
+            className="form-submit-button"
+            disabled={loading}
           >
-          { type === "Sign In" ? " Sign Up" : " Sign In"}
-          </Link>
-        </div>
-      </form>
-    </Form>
+            {!loading ? type === "Sign In" ? "Sign In" : "Sign Up" : <></>}
+            {loading && (
+              <Image
+                src="/assets/icons/loader.svg"
+                alt="loading"
+                width={24}
+                height={24}
+                className="ml-2 animate-spin"
+              />
+            )}
+          </Button>
+          {errorMessage && <p className="error-message">*{errorMessage}</p>}
+
+          <div className="body-2 flex justify-center">
+            <p className="text-light-100">
+              {type === "Sign In"
+                ? " Don't have an account?"
+                : "Already have an account?"}
+            </p>
+            <Link
+              href={type === "Sign In" ? "/sign-up" : "/sign-in"}
+              className="ml-1 font-medium text-brand"
+            >
+              {type === "Sign In" ? " Sign Up" : " Sign In"}
+            </Link>
+          </div>
+        </form>
+      </Form>
+      {accountId && (
+        <OTPModal email={form.getValues("email")} accountId={accountId} />
+      )}
+    </>
   );
 };
 
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -5,6 +5,7 @@ import { createAdminClient } from "../appwrite";
 import { appwriteConfig } from "../appwrite/config";
 import { avatarPlaceholderUrl } from "@/constants";
 import { parseStringify } from "../utils";
+import { cookies } from "next/headers";
 
 const handleError = (error: unknown, message: string) => {
   console.log(error, message);
@@ -64,3 +65,21 @@ export const createAccount = async ({
 
   return parseStringify({ accountId });
 };
+
+export const verifySecret = async ({ accountId, password }: {accountId: string | null, password: string}) => {
+  try {
+    const { account } = await createAdminClient();
+    const session = await account.createSession(accountId || '', password);
+
+    (await cookies()).set("appwrite-session",session.secret, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+      secure: true
+    });
+
+    return parseStringify({ sessionId: session.$id });
+  } catch (error) {
+    handleError(error, "Failed to verify OTP, wrong OTP");
+  }
+}
