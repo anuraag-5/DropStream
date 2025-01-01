2470d4b5f9c50e8bd3445cdc27c576ae07990986
Anurag bhoite <anuragcr7778@gmail.com>
2025-01-01 21:10:37 +0530

    ...

--- components/Card.tsx
+++ components/Card.tsx
@@ -4,10 +4,11 @@ import React from "react";
 import Thumbnail from "./Thumbnail";
 import { convertFileSize } from "@/lib/utils";
 import FormattedDateTime from "./FormattedDateTime";
+import ActionDropdown from "./ActionDropdown";
 
 const Card = ({ file }: { file: Models.Document }) => {
   return (
-    <Link href={file.url} target="_blank" className="file-card">
+  <Link href={file.url} target="_blank" className="file-card">
       <div className="flex justify-between">
         <Thumbnail
           type={file.type}
@@ -17,14 +18,17 @@ const Card = ({ file }: { file: Models.Document }) => {
           imageClassname="!size-11"
         />
         <div className="flex flex-col items-end justify-between">
-          ActionsDropdown ...
+          <ActionDropdown file={file}/>
           <p className="body-1">{convertFileSize(file.size)}</p>
         </div>
       </div>
 
       <div className="file-card-details break-words">
         <p className="subtitle-2 line-clamp-1">{file.name}</p>
-        <FormattedDateTime />
+        <FormattedDateTime date={file.$createdAt} className="body-2 text-light-100"/>
+        <p className=" caption line-clamp-1 text-light-200">
+          By: {file.owner.fullName}
+        </p>
       </div>
     </Link>
   );
